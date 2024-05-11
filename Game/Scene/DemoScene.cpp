f63fc25d17a56a026af32b968f8100a8ea1d54e6
MatsubaraKai <k022g0104@g.neec.ac.jp>
2024-05-11 14:49:56 +0900

    完成

--- Game/Scene/DemoScene.cpp
+++ Game/Scene/DemoScene.cpp
@@ -13,6 +13,7 @@ void DemoScene::Init()
 	input = Input::GetInstance();
 	textureHandle = TextureManager::StoreTexture("Resources/uvChecker.png");
 	textureHandle2 = TextureManager::StoreTexture("Resources/white.png");
+	textureHandle3 = TextureManager::StoreTexture("Resources/AnimatedCube/AnimatedCube_BaseColor.png");
 	demoSprite = new Sprite();
 	demoSprite->Init({ 0.0f,0.0f }, { 600.0f,600.0f }, { 0.0f,0.0f }, { 1.0f,1.0f,1.0f,1.0f }, "Resources/uvChecker.png");
 	material.color = { 1.0f,1.0f,1.0f,1.0f };
@@ -21,18 +22,25 @@ void DemoScene::Init()
 	worldTransform.translation_.x = 0;
 	worldTransform2.Initialize();
 	worldTransform2.translation_.x = 5;
+	worldTransform3.Initialize();
+	worldTransform3.translation_.x = 2;
 	worldTransform.UpdateMatrix();
 	worldTransform2.UpdateMatrix();
+	worldTransform3.UpdateMatrix();
 
 	ModelManager::GetInstance()->LoadModel("Resources/plane", "plane.gltf");
 	ModelManager::GetInstance()->LoadModel("Resources/ball", "ball.obj");
+	ModelManager::GetInstance()->LoadModel("Resources/AnimatedCube", "AnimatedCube.gltf");
 	object3d = new Object3d();
 	object3d->Init();
 	object3d2 = new Object3d();
 	object3d2->Init();
+	object3d3 = new Object3d();
+	object3d3->Init();
 
 	object3d->SetModel("plane.gltf");
 	object3d2->SetModel("ball.obj");
+	object3d3->SetModel("AnimatedCube.gltf");
 	particle = new Particle();
 	particle2 = new Particle();
 
@@ -69,11 +77,14 @@ void DemoScene::Update()
 	camera->Update();
 	demoSprite->Update();
 	worldTransform.rotation_.y += 0.05f;
+	worldTransform3.rotation_.y += 0.05f;
 	object3d->SetWorldTransform(worldTransform);
 	object3d2->SetWorldTransform(worldTransform2);
+	object3d3->SetWorldTransform(worldTransform3);
 
 	object3d->Update();
 	object3d2->Update();
+	object3d3->Update();
 
 }
 void DemoScene::Draw()
@@ -81,6 +92,7 @@ void DemoScene::Draw()
 	//demoSprite->Draw(textureHandle,{1.0f,1.0f,1.0f,1.0f});
 	object3d->Draw(textureHandle, camera);
 	object3d2->Draw(textureHandle2, camera);
+	object3d3->Draw(textureHandle3, camera);
 	particle->Draw(demoEmitter_, { worldTransform.translation_.x,worldTransform.translation_.y,worldTransform.translation_.z + 5 }, textureHandle, camera, demoRandPro, false);
 	particle2->Draw(demoEmitter_, { worldTransform2.translation_.x,worldTransform2.translation_.y,worldTransform2.translation_.z + 5 }, textureHandle2, camera, demoRandPro, false);
 }
