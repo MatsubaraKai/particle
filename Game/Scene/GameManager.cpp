6ff2dc5e0df26a4902c53ac9bec28b9fc5d3ba7b
MatsubaraKai <k022g0104@g.neec.ac.jp>
2024-07-11 11:10:33 +0900

    a

--- Game/Scene/GameManager.cpp
+++ Game/Scene/GameManager.cpp
@@ -161,6 +161,9 @@ int GameManager::Run() {
 			currentStatus = "Test2 effect applied";
 		}
 		ImGui::End();
+		ImGui::Begin("FPS");
+		ImGui::Text("%f", ImGui::GetIO().Framerate);
+		ImGui::End();
 
 		// シーンのチェック
 		prevSceneNo_ = currentSceneNo_;
